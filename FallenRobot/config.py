bdde67251896801ea6731e24be831d2d170154d0
Monster dragon <165640633+Sumitmeena12@users.noreply.github.com>
2024-05-27 15:37:04 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,7 +14,7 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://te.legra.ph/file/82f1053c956dd6256ff1e.jpg"
+    START_IMG = "https://telegra.ph/file/d5d88fe2520e09e32ef07.jpg"
 
     SUPPORT_CHAT = "BRANDED_WORLD"  # Your Telegram support group chat username where your users will go and bother you
 
@@ -22,7 +22,7 @@ class Config(object):
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 6258877205  # User id of your telegram account (Must be integer)
+    OWNER_ID = 5569528945  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
